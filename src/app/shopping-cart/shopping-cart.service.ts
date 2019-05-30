bcd630c7256b1f2681ecf8f02f2d52f23439d9d2
Aaron Harrison <aaronharrison16@gmail.com>
2019-05-29 20:21:08 -0600

    added user association with the items as well as restructured the database org

--- src/app/shopping-cart/shopping-cart.service.ts
+++ src/app/shopping-cart/shopping-cart.service.ts
@@ -1,32 +1,39 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable()
 export class ShoppingCartService {
 
-  constructor(public db:AngularFirestore) { }
+  userId: string
+
+  constructor(public db:AngularFirestore, private afAuth:AngularFireAuth) { 
+    this.afAuth.authState.subscribe(user => {
+      this.userId = user.uid
+    })
+  }
 
   getCartItems() {
-    return this.db.collection('shopping-cart').snapshotChanges();
+    return this.db.collection(`items/${this.userId}/shopping-cart`).snapshotChanges();
   }
 
   addItem(name) {
-    return this.db.collection('shopping-cart').add({
+    return this.db.collection(`items/${this.userId}/shopping-cart`).add({
       name: name
     })
   }
 
   deleteCartItem(itemKey) {
-    return this.db.collection('shopping-cart').doc(itemKey).delete()
+    return this.db.collection(`items/${this.userId}/shopping-cart`).doc(itemKey).delete()
   }
 
   updateCartItem( itemKey, value ) {
-    return this.db.collection('shopping-cart').doc(itemKey).set({ name: value });
+    return this.db.collection(`items/${this.userId}/shopping-cart`).doc(itemKey).set({ name: value });
   }
 
   clearCartItems(cartItems) {
     for (let item of cartItems) {
-      this.db.collection('shopping-cart').doc(item.payload.doc.id).delete();
+      this.db.collection(`items/${this.userId}/shopping-cart`).doc(item.payload.doc.id).delete();
     }
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,37 +1,45 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable()
 export class ShoppingListService {
+
+  userId: string;
   
-  constructor(public db:AngularFirestore) { }
+  constructor(public db:AngularFirestore, private afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe(user => {
+      this.userId = user.uid
+    })
+  }
 
   getItems() {
-    return this.db.collection('shopping-list').snapshotChanges();
+    console.log(this.userId)
+    return this.db.collection(`items/${this.userId}/shopping-list`).snapshotChanges();
   }
 
   addItem(value) {
-    return this.db.collection('shopping-list').add({
-      name: value.value.name
+    return this.db.collection(`items/${this.userId}/shopping-list`).add({
+      name: value.value.name,
     })
   }
 
   deleteItem(itemKey) {
-    return this.db.collection('shopping-list').doc(itemKey).delete();
+    return this.db.collection(`items/${this.userId}/shopping-list`).doc(itemKey).delete();
   }
 
   updateItem(itemKey, value) {
-    return this.db.collection('shopping-list').doc(itemKey).set({name: value});
+    return this.db.collection(`items/${this.userId}/shopping-list`).doc(itemKey).set({name: value});
   }
 
   clearItems(items) {
     for(let item of items) {
-      this.db.collection('shopping-list').doc(item.payload.doc.id).delete();  
+      this.db.collection(`items/${this.userId}/shopping-list`).doc(item.payload.doc.id).delete();  
     }
   }
 
   returnToList(item) {
-    return this.db.collection('shopping-list').add({
+    return this.db.collection(`items/${this.userId}/shopping-list`).add({
       name: item
     })
   }
