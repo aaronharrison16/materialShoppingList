81eea5bb83346a7db18c605bcf4b987d879d8787
Aaron Harrison <aaronharrison16@gmail.com>
2019-06-01 12:57:16 -0600

    added the auth guard to deny access to non users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from "@angular/router";
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AuthGuard } from './auth.guard';
 
 const appRoutes: Routes = [
   { path: '', component: LandingPageComponent },
-  { path: 'shopping-list', component: ShoppingListComponent },
-  { path: 'shopping-cart', component: ShoppingCartComponent }
+  { path: 'shopping-list', component: ShoppingListComponent, canActivate: [AuthGuard] },
+  { path: 'shopping-cart', component: ShoppingCartComponent, canActivate: [AuthGuard] }
 ]
 
 
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './core/auth.service';
+import { tap, take, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  
+  canActivate(next, state): Observable<boolean> {
+    return this.authService.user$.pipe(
+      take(1),
+      map(user => !!user),
+      tap(loggedIn => {
+        if (!loggedIn) {
+          console.log('access denied');
+          this.router.navigate(['/'])
+        }
+      })
+    )
+  }
+}
