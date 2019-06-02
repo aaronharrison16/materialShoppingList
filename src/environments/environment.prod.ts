90243bf12f0760b9959ad0c443cdf0a81334356b
Aaron Harrison <aaronharrison16@gmail.com>
2019-06-02 16:56:00 -0600

    changed production enviornment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAwLh_gASPmyXTo-e6RiAAOmvZJEkzDIQQ",
+    authDomain: "shopping-list-material.firebaseapp.com",
+    databaseURL: "https://shopping-list-material.firebaseio.com",
+    projectId: "shopping-list-material",
+    storageBucket: "shopping-list-material.appspot.com",
+    messagingSenderId: "416690021468"
+  }
 };
